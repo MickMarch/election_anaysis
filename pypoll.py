dff90b7a65d70f12c6a5d7a0ef38c4ace62e4ce5
Mickadoodoo <michael.r.marchand@gmail.com>
2022-12-13 18:47:29 -0500

    Updating Repo + .gitignore

---
+++ pypoll.py
@@ -0,0 +1,7 @@
+# In this project, our final Python script will need to be able to deliver the following information when the script is run:
+
+# 1. Total number of votes cast
+# 2. A complete list of candidates who received votes
+# 3. Total number of votes each candidate received
+# 4. Percentage of votes each candidate won
+# 5. The winner of the election based on popular vote
